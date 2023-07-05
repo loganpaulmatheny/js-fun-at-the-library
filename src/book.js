5f112bbba2fb1c901eece52952f09265d75a1808
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-05 14:28:18 -0400

    Pass createTitle tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,14 @@
-function createTitle() {
-  
+function createTitle(title) {
+  var modifiedTitle = `The ${title}`;
+  return modifiedTitle;
 }
+function buildMainCharacter() {}
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
+  buildMainCharacter,
   // saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
-}
+};
