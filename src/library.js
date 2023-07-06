3bc6c65efe264f3eb70069134be2416eac99ae91
Logan Matheny <loganpaulmatheny@gmail.com>
2023-07-06 07:55:16 -0400

    Pass all tests within shelf-test.js

--- src/library.js
+++ src/library.js
@@ -1,8 +1,49 @@
+const { shelfBook, unshelfBook, searchShelf } = require(`./shelf.js`);
 
+function createLibrary(libraryName) {
+  var library = {
+    name: libraryName,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+  return library;
+}
 
+function addBook(library, book) {
+  // we know the genre which determines shelf
+  // select where it is going
+  // call shelfBook from previous module
+  var shelf = book.genre;
+  // console.log(shelf);
+  var libraryShelf = library.shelves[shelf];
+  // console.log(libraryShelf);
+  shelfBook(book, libraryShelf);
+}
+
+function checkoutBook(library, title, genre) {
+  // check to see if it's available
+  // unshelf works by using the title and shelf
+  // get the title of the book
+  // get the shelf at the library
+  // unshelf book
+  var status = "";
+  var libraryShelf = library.shelves[genre];
+  var available = searchShelf(libraryShelf, title);
+  if (available === true) {
+    var updatedShelf = unshelfBook(title, libraryShelf);
+    status = `You have now checked out ${title} from the ${library.name}.`;
+    return status;
+  } else {
+    status = `Sorry, there are currently no copies of ${title} available at the ${library.name}.`;
+    return status;
+  }
+}
 module.exports = {
-  // createLibrary,
-  // addBook,
-  // checkoutBook,
+  createLibrary,
+  addBook,
+  checkoutBook,
   // takeStock
 };
--- src/shelf.js
+++ src/shelf.js
@@ -32,9 +32,25 @@ function listTitles(shelf) {
 
 // The best way I can think of to solve the 'join' problem, which is that you need the titles of the objects all in a string, but you can't have a comma on the last index (ergo you can't just do string interpolation easily) is to put all the titles in their own array and then join them.
 
+function searchShelf(shelf, title) {
+  var found = shelf.some((book) => book.title === title);
+  console.log(found);
+  if (found === true) {
+    console.log("Yes that book is in 📚");
+  } else {
+    console.log(
+      `I'm sorry it looks like that book is checked out or missing 💨`
+    );
+  }
+  return found;
+}
+// My function was not working earlier because I was also trying to print the titles from the objects within the array. However, if no title exists there will be nothing to print - this was causing my function to have irregular behavior.
+
 module.exports = {
   shelfBook,
   unshelfBook,
   listTitles,
-  // searchShelf
+  searchShelf,
 };
+
+// Refactor - the code should DEFINITELY be reafactored specifically you should search the shelf before unshelfing a book as a well before shelfing it (maybe there's already a copy). We could simply alter the code a bit to include those callback functions within shelfBook() and unshelfBook() to take this into account. Currently the tests don't check for this case.
